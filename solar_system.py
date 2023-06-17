c771c6f44a09195dbd2c0122e8b2b40742ae5c61
Pablo Vaz <64449448+fisicamaldonado@users.noreply.github.com>
2023-06-17 18:45:49 -0300

    Update solar_system.py

--- solar_system.py
+++ solar_system.py
@@ -10,19 +10,26 @@ Each planet's information is stored as a list, with the values representing:
 3. Gravity (in m/s²)
 4. Rotation period (in Earth hours)
 5. Orbital period around the sun (in Earth days)
-6. Distance from the sun (in UA)
+6. Semimajor axis: It is expressed in astronomical units (AU).
+7. Eccentricity: Dimentionless value between 0 and 1.
+8. Inclination: The inclination of the planet's axial tilt with respect to its orbital plane, measured in degrees.
+9. Density: The estimated average density of the planet, measured in grams per cubic centimeter (g/cm³).
+10. Atmospheric Composition: The composition of the planet's atmosphere, described as a string.
+11. Number of Moons: The approximate number of moons orbiting the planet.
+
 '''
 
 solar_system = {
-    'Mercury': [0.055, 0.383, 3.7, 58.6, 88, 0.39],
-    'Venus': [0.815, 0.949, 8.87, -243, 225, 0.72],
-    'Earth': [1.0, 1.0, 9.81, 1, 365.25, 1.0],
-    'Mars': [0.107, 0.532, 3.71, 1.03, 687, 1.52],
-    'Jupiter': [318, 11.209, 24.79, 0.41, 4332, 5.20],
-    'Saturn': [95, 9.449, 10.44, 0.43, 10759, 9.58],
-    'Uranus': [14.5, 4.007, 8.69, -0.72, 30688, 19.18],
-    'Neptune': [17.1, 3.883, 11.15, 0.67, 60182, 30.07]
+    'Mercury': [0.055, 0.383, 3.7, 58.6, 88, 0.39, 0.387, 0.205, 0.0, 5.427, 'N/A', 0],
+    'Venus': [0.815, 0.949, 8.87, -243, 225, 0.72, 0.723, 0.007, 177.4, 5.243, 'Carbon Dioxide (96.5%), Nitrogen (3.5%)', 0],
+    'Earth': [1.0, 1.0, 9.81, 1, 365.25, 1.0, 1.0, 0.017, 23.45, 5.515, 'Nitrogen (78.1%), Oxygen (20.9%)', 1],
+    'Mars': [0.107, 0.532, 3.71, 1.03, 687, 1.52, 1.524, 0.094, 23.98, 3.933, 'Carbon Dioxide (95.3%), Nitrogen (2.7%)', 2],
+    'Jupiter': [318, 11.209, 24.79, 0.41, 4332, 5.20, 5.203, 0.049, 3.08, 1.326, 'Hydrogen (90%), Helium (10%)', 79],
+    'Saturn': [95, 9.449, 10.44, 0.43, 10759, 9.58, 9.582, 0.057, 26.73, 0.687, 'Hydrogen (96%), Helium (3%)', 82],
+    'Uranus': [14.5, 4.007, 8.69, -0.72, 30688, 19.18, 19.182, 0.046, 97.92, 1.27, 'Hydrogen (83%), Helium (15%)', 27],
+    'Neptune': [17.1, 3.883, 11.15, 0.67, 60182, 30.07, 30.070, 0.011, 28.8, 1.638, 'Hydrogen (80%), Helium (19%)', 14]
 }
+
 
 # Extract the masses and radii of the planets from the dictionary
 mass = np.array(list(solar_system.values()))[:, 0]
